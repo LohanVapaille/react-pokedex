64e486b18e4e457d69011418a55f580071d6a40f
LohanVapaille <lohanvapaille@gmail.com>
2026-02-12 20:48:40 -0500

    vite change

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Ajoute cette ligne avec le nom EXACT de ton dépôt GitHub entre les slashs
+  base: '/react-pokedex/',
 })

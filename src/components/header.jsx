67f4e0b0def4064e6e5fd8caba3d33252175ca7c
LohanVapaille <lohanvapaille@gmail.com>
2026-02-12 21:13:52 -0500

    header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="header">
-      <img src="/pokeball.png" alt="Pokeball" width="50" />
+      <img src="./pokeball.png" alt="Pokeball" width="50" />
       <h1>Pokedex React </h1>
     </header>
   );
